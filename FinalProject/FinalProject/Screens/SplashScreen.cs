c3856543ffb6d8fb4adef762a26cec2b34a228c3
admiralsam <samuel.dong@outlook.com>
2015-03-29 16:26:16 -0500

    Added methods to SplashScreen

--- FinalProject/FinalProject/Screens/SplashScreen.cs
+++ FinalProject/FinalProject/Screens/SplashScreen.cs
@@ -5,7 +5,39 @@ using System.Text;
 
 namespace FinalProject.Screens
 {
-    class SplashScreen
+    class SplashScreen : Screen
     {
+        public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+        protected override void LoadContent()
+        {
+            throw new NotImplementedException();
+        }
+        protected override void ScreenUpdate(float secondsPassed)
+        {
+            throw new NotImplementedException();
+        }
+        public override void Reset()
+        {
+            base.Reset();
+        }
+        protected override void Set()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Start()
+        {
+            base.Start();
+        }
+        public override void Stop()
+        {
+            base.Stop();
+        }
+        public override void TransitionOut()
+        {
+            base.TransitionOut();
+        }
     }
 }
