f97aafc1d95204a0ead51a140ce5eaed90fd273c
admiralsam <samuel.dong@outlook.com>
2015-04-09 20:14:12 -0500

    Added CommandCenter Back In

---
+++ FinalProject/FinalProject/Screens/CommandCenterScreen.cs
@@ -0,0 +1,161 @@
+﻿using FinalProject.GameResources;
+using FinalProject.Utilities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FinalProject.Screens
+{
+    internal class CommandCenterScreen : Screen
+    {
+        public SaveGame currentGame;
+        public ScreenEvent FinishedTransitioningOut;
+        public ScreenEvent StartingTransitioningOut;
+        private Texture2D background;
+        private bool firstIteration;
+        private MenuItemGroup menuItems;
+        private InterpolatedValue scaleIn, scaleOut;
+        private string selected;
+        private Texture2D snapshot;
+
+        public CommandCenterScreen(ContentManager contentManager, GraphicsDevice graphicsDevice)
+            : base(contentManager, graphicsDevice)
+        {
+            scaleIn = new ExponentialInterpolatedValue(.002f, .25f, .5f);
+            scaleIn.InterpolationFinished = ScaleInFinished;
+            scaleOut = new ExponentialInterpolatedValue(.25f, .002f, .5f);
+            scaleOut.InterpolationFinished = ScaleOutFinished;
+            menuItems = new MenuItemGroup();
+            menuItems.AddItem(new MenuItem(new Vector2(280, 320), "LEVEL SELECT"));
+            menuItems.AddItem(new MenuItem(new Vector2(280, 450), "UPGRADES"));
+            selected = "";
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            switch (state)
+            {
+                case ScreenState.TransitioningIn:
+                    {
+                        if (firstIteration)
+                        {
+                            firstIteration = false;
+                            GraphicsUtilities.BeginDrawingToTexture(spriteBatch, graphicsDevice);
+                            DrawScreen(spriteBatch);
+                            GraphicsUtilities.EndDrawingToTexture(spriteBatch, graphicsDevice);
+                            snapshot = GraphicsUtilities.DuplicateTexture(GraphicsUtilities.GetTexture(), graphicsDevice);
+                        }
+                        GraphicsUtilities.DrawPixelatedTexture(spriteBatch, snapshot, Vector2.Zero, scaleIn.GetValue(), graphicsDevice);
+                    } break;
+                case ScreenState.Active:
+                    {
+                        GraphicsUtilities.BeginDrawingToTexture(spriteBatch, graphicsDevice);
+                        DrawScreen(spriteBatch);
+                        GraphicsUtilities.EndDrawingToTexture(spriteBatch, graphicsDevice);
+                        spriteBatch.Draw(GraphicsUtilities.GetTexture(), new Rectangle(0, 0, Constants.VirtualWidth, Constants.VirtualHeight), Color.White);
+                    } break;
+                case ScreenState.TransitioningOut:
+                    {
+                        GraphicsUtilities.DrawPixelatedTexture(spriteBatch, snapshot, Vector2.Zero, scaleOut.GetValue(), graphicsDevice);
+                    } break;
+            }
+        }
+
+        public override void KeyPressed(Keys key)
+        {
+            switch (state)
+            {
+                case ScreenState.Active:
+                    {
+                        switch (key)
+                        {
+                            case Keys.Enter:
+                                {
+                                    selected = menuItems.GetSelected();
+                                    StartingTransitioningOut(selected);
+                                } break;
+                            case Keys.Up:
+                                {
+                                    menuItems.MoveUp();
+                                } break;
+                            case Keys.Down:
+                                {
+                                    menuItems.MoveDown();
+                                } break;
+                            case Keys.Escape:
+                                {
+                                    StartingTransitioningOut("");
+                                } break;
+                        }
+                    } break;
+            }
+        }
+
+        public override void LoadContent()
+        {
+            background = content.Load<Texture2D>("MenuBackground");
+            base.LoadContent();
+        }
+
+        public override void Reset()
+        {
+            scaleIn.SetParameter(0);
+            scaleOut.SetParameter(0);
+            selected = "";
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Stop()
+        {
+            base.Stop();
+        }
+
+        public override void TransitionOut()
+        {
+            snapshot = GraphicsUtilities.DuplicateTexture(GraphicsUtilities.GetTexture(), graphicsDevice);
+            base.TransitionOut();
+        }
+
+        protected override void ScreenUpdate(float secondsPassed)
+        {
+            switch (state)
+            {
+                case ScreenState.TransitioningIn:
+                    {
+                        scaleIn.Update(secondsPassed);
+                    } break;
+                case ScreenState.TransitioningOut:
+                    {
+                        scaleOut.Update(secondsPassed);
+                    } break;
+            }
+        }
+
+        protected override void Set()
+        {
+            firstIteration = true;
+        }
+
+        private void DrawScreen(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(background, new Rectangle(0, 0, Constants.VirtualWidth, Constants.VirtualHeight), Color.White);
+            menuItems.Draw(spriteBatch);
+            GraphicsUtilities.DrawStringVerticallyCentered(spriteBatch, Fonts.MenuTitle, "COMMAND CENTER", new Vector2(320, 210), Fonts.Green);
+        }
+
+        private void ScaleInFinished(float parameter)
+        {
+            state = ScreenState.Active;
+        }
+
+        private void ScaleOutFinished(float parameter)
+        {
+            FinishedTransitioningOut(selected);
+        }
+    }
+}
