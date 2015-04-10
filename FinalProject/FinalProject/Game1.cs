2ff0df3dd5bc9bc8bf67c76294e1a9ffbe8fafdf
admiralsam <samuel.dong@outlook.com>
2015-04-09 20:08:05 -0500

    Fixed Missing SpriteFont and Added CommandCenter

--- FinalProject/FinalProject/Game1.cs
+++ FinalProject/FinalProject/Game1.cs
@@ -13,6 +13,7 @@ namespace FinalProject
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        private CommandCenterScreen commandCenterScreen;
         private Screen current, next;
         private GraphicsDeviceManager graphics;
         private Keys[] lastPressedKeys;
@@ -73,6 +74,9 @@ namespace FinalProject
             loadGameScreen = new LoadGameScreen(GameUtilities.GenerateNewContentManager(Services), GraphicsDevice);
             loadGameScreen.FinishedTransitioningOut = LoadGameScreenFinishedTransitioningOut;
             loadGameScreen.StartingTransitioningOut = LoadGameScreenStartingTransitioningOut;
+            commandCenterScreen = new CommandCenterScreen(GameUtilities.GenerateNewContentManager(Services), GraphicsDevice);
+            commandCenterScreen.StartingTransitioningOut = CommandCenterStartingTransitioningOut;
+            commandCenterScreen.FinishedTransitioningOut = CommandCenterFinishedTransitioningOut;
             //Testing Purposes
             for (int i = 0; i < 23; i++)
             {
@@ -144,6 +148,32 @@ namespace FinalProject
             lastPressedKeys = Keyboard.GetState().GetPressedKeys();
         }
 
+        private void CommandCenterFinishedTransitioningOut(string message)
+        {
+            switch (message)
+            {
+                case "":
+                    {
+                        current = mainMenuScreen;
+                        mainMenuScreen.Start();
+                    } break;
+            }
+            commandCenterScreen.Stop();
+            commandCenterScreen.UnloadContent();
+        }
+
+        private void CommandCenterStartingTransitioningOut(string message)
+        {
+            switch (message)
+            {
+                case "":
+                    {
+                        mainMenuScreen.LoadContentAsynchronously();
+                    } break;
+            }
+            commandCenterScreen.TransitionOut();
+        }
+
         private void LoadGameScreenFinishedTransitioningOut(string message)
         {
             switch (message)
@@ -155,7 +185,9 @@ namespace FinalProject
                     } break;
                 default:
                     {
-                        current = null;
+                        current = commandCenterScreen;
+                        commandCenterScreen.currentGame = SaveGameManager.GetSavedGame(message + ".sav");
+                        commandCenterScreen.Start();
                     } break;
             }
             loadGameScreen.Stop();
@@ -172,7 +204,7 @@ namespace FinalProject
                     } break;
                 default:
                     {
-                        SaveGameManager.GetSavedGame(message + ".sav");
+                        commandCenterScreen.LoadContentAsynchronously();
                     } break;
             }
             loadGameScreen.TransitionOut();
