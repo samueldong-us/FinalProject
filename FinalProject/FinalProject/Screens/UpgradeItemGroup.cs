51c60c4fb11b018a12d713aa912bab375989210b
admiralsam <samuel.dong@outlook.com>
2015-04-09 21:17:41 -0500

    Added UpgradeItemGroup

--- FinalProject/FinalProject/Screens/UpgradeItemGroup.cs
+++ FinalProject/FinalProject/Screens/UpgradeItemGroup.cs
@@ -5,7 +5,27 @@ using System.Text;
 
 namespace FinalProject.Screens
 {
-    class UpgradeItemGroup
+    internal class UpgradeItemGroup : MenuItemGroup
     {
+        public Dictionary<string, int> GetLevels()
+        {
+            Dictionary<string, int> upgrades = new Dictionary<string, int>();
+            foreach (MenuItem item in items)
+            {
+                UpgradeItem upgradeItem = (UpgradeItem)item;
+                upgrades[upgradeItem.Text] = upgradeItem.level;
+            }
+            return upgrades;
+        }
+
+        public int GetSelectedCost()
+        {
+            return ((UpgradeItem)items[index]).GetCost();
+        }
+
+        public void UpgradeSelected()
+        {
+            ((UpgradeItem)items[index]).level++;
+        }
     }
 }
