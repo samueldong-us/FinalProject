dfcc6b41ad28f0cc4f0e733155545a0e3300031d
Al <van.one.one@gmail.com>
2015-04-16 21:53:13 -0500

    boundingarea added, weaon component started

---
+++ FinalProject/FinalProject/GameResources/TransformBoudSignaler.cs
@@ -0,0 +1,32 @@
+﻿using FinalProject.Messaging;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalProject.GameResources
+{
+    internal class TransformBoundSignaler : TransformComponent
+    {
+        private Rectangle boundingBox;
+        private Entity currentGameObject;
+        private MessageCenter messageOut;
+
+        public TransformBoundSignaler(MessageCenter messageCenter, MessageCenter messageOut, int leftBound, int rightBound, int upperBound, int lowerBound, Entity gameWorldObject)
+            : base(messageCenter)
+        {
+            boundingBox = new Rectangle(leftBound, upperBound, rightBound-leftBound, lowerBound-upperBound);
+            currentGameObject = gameWorldObject;
+            this.messageOut = messageOut;
+        }
+
+        public override void Update(float secondsPassed)
+        {
+            base.Update(secondsPassed);
+            if (!boundingBox.Contains((int) Position.X, (int) Position.Y)) {
+                messageOut.Broadcast<Entity>("out of bounds", currentGameObject);
+            }
+        }
+    }
+}
---
+++ FinalProject/FinalProject/GameResources/WeaponComponent.cs
@@ -0,0 +1,37 @@
+﻿using FinalProject.Messaging;
+using FinalProject.Screens;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalProject.GameResources
+{
+    internal class WeaponComponent : Component
+    {
+        private List<Entity> bullets;
+        private MessageCenter weaponMessager;
+        private TransformComponent playerShipInfo;
+        private Texture2D bulletImage;
+
+        public WeaponComponent(MessageCenter entityMessageCenter, TransformComponent inputShipInfo, Texture2D inputBulletImage)
+            : base(entityMessageCenter)
+        {
+            bullets = new List<Entity>();
+            weaponMessager = new MessageCenter();
+            playerShipInfo = inputShipInfo;
+            bulletImage = inputBulletImage;
+        }
+
+        private Entity makeBullets()
+        {
+            Entity bullet = new Entity();
+            TransformBoundSignaler bulletBounds = new TransformBoundSignaler();
+            return bullet;
+        }
+       
+    }
+}
