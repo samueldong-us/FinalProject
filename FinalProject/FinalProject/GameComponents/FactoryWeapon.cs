c4c99e7d4269204c9be51dc387c8a4ac41fe4c23
CrustMaster420XxXilluminatiXxX <lilcrazykid0@hotmail.com>
2015-05-04 23:41:09 -0500

    Weapons And Factory And More !!!!!

--- FinalProject/FinalProject/GameComponents/FactoryWeapon.cs
+++ FinalProject/FinalProject/GameComponents/FactoryWeapon.cs
@@ -17,7 +17,55 @@ namespace FinalProject.GameComponents
                     {
                         AddFanWeapon(entity, difficulty, stage);
                     } break;
+                case "Bullet":
+                    {
+                        AddBulletWeapon(entity, difficulty, stage);
+                    } break;
+                case "Bullet Stream":
+                    {
+                        AddBulletStreamWeapon(entity, difficulty, stage);
+                    } break;
+                case "Circular Bounce":
+                    {
+                        AddCircularBounceWeapon(entity, difficulty, stage);
+                    } break;
+                case "Fan Stream":
+                    {
+                        AddFanStreamWeapon(entity, difficulty, stage);
+                    } break;
+                case "Split Shot":
+                    {
+                        AddSplitShotWeapon(entity, difficulty, stage);
+                    } break;
             }
+        }
+
+        private static void AddBulletStreamWeapon(Entity entity, SaveGame.Difficulty difficulty, int stage)
+        {
+            float fireRate = (float)Values.WeaponValues[difficulty][stage]["Bullet Stream"]["Fire Rate"];
+            int numberOfBullets = (int)Values.WeaponValues[difficulty][stage]["Bullet Stream"]["Number Of Bullets"];
+            int damage = (int)Values.WeaponValues[difficulty][stage]["Bullet Stream"]["Damage"];
+            new ComponentConstantRateFire(entity, fireRate);
+            new ComponentProjectileWeaponBulletStream(entity, numberOfBullets, damage);
+        }
+
+        private static void AddBulletWeapon(Entity entity, SaveGame.Difficulty difficulty, int stage)
+        {
+            float fireRate = (float)Values.WeaponValues[difficulty][stage]["Bullet"]["Fire Rate"];
+            int numberOfBullets = (int)Values.WeaponValues[difficulty][stage]["Bullet"]["Number Of Bullets"];
+            int damage = (int)Values.WeaponValues[difficulty][stage]["Bullet"]["Damage"];
+            new ComponentConstantRateFire(entity, fireRate);
+            new ComponentProjectileWeaponBullet(entity, numberOfBullets, damage);
+        }
+
+        private static void AddCircularBounceWeapon(Entity entity, SaveGame.Difficulty difficulty, int stage)
+        {
+            float fireRate = (float)Values.WeaponValues[difficulty][stage]["Circular Bounce"]["Fire Rate"];
+            int numberOfBullets = (int)Values.WeaponValues[difficulty][stage]["Circular Bounce"]["Number Of Bullets"];
+            int damage = (int)Values.WeaponValues[difficulty][stage]["Circular Bounce"]["Damage"];
+            float rotationalDelta = (float)Values.WeaponValues[difficulty][stage]["Circular Bounce"]["Rotational Delta"];
+            new ComponentConstantRateFire(entity, fireRate);
+            new ComponentProjectileWeaponCircularBounce(entity, numberOfBullets, damage, rotationalDelta);
         }
 
         private static void AddCircularFireWeapon(Entity entity, SaveGame.Difficulty difficulty, int stage)
@@ -30,6 +78,15 @@ namespace FinalProject.GameComponents
             new ComponentProjectileWeaponCircularFire(entity, numberOfBullets, damage, rotationalDelta);
         }
 
+        private static void AddFanStreamWeapon(Entity entity, SaveGame.Difficulty difficulty, int stage)
+        {
+            float fireRate = (float)Values.WeaponValues[difficulty][stage]["Fan Stream"]["Fire Rate"];
+            int numberOfBullets = (int)Values.WeaponValues[difficulty][stage]["Fan Stream"]["Number Of Bullets"];
+            int damage = (int)Values.WeaponValues[difficulty][stage]["Fan Stream"]["Damage"];
+            new ComponentConstantRateFire(entity, fireRate);
+            new ComponentProjectileWeaponFanStream(entity, numberOfBullets, damage);
+        }
+
         private static void AddFanWeapon(Entity entity, SaveGame.Difficulty difficulty, int stage)
         {
             float fireRate = (float)Values.WeaponValues[difficulty][stage]["Fan"]["Fire Rate"];
@@ -37,6 +94,15 @@ namespace FinalProject.GameComponents
             int damage = (int)Values.WeaponValues[difficulty][stage]["Fan"]["Damage"];
             new ComponentConstantRateFire(entity, fireRate);
             new ComponentProjectileWeaponFan(entity, numberOfBullets, damage);
+        }
+
+        private static void AddSplitShotWeapon(Entity entity, SaveGame.Difficulty difficulty, int stage)
+        {
+            float fireRate = (float)Values.WeaponValues[difficulty][stage]["Split Shot"]["Fire Rate"];
+            int numberOfBullets = (int)Values.WeaponValues[difficulty][stage]["Split Shot"]["Number Of Bullets"];
+            int damage = (int)Values.WeaponValues[difficulty][stage]["Split Shot"]["Damage"];
+            new ComponentConstantRateFire(entity, fireRate);
+            new ComponentProjectileWeaponSplitShot(entity, numberOfBullets, damage);
         }
     }
 }
